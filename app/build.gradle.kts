8e343557321d474035c771c453b8f0512526df13
mppjuro <mppjuro@gmail.com>
2026-08-18 21:48:10 +0200

    deleted proguard related settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,10 +20,6 @@ android {
         release {
             isMinifyEnabled = true
             isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
     compileOptions {
